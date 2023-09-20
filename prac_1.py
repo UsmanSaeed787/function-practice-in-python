2386c70237a431f696758266c93e671b756a4c73
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-09-20 15:58:53 +0500

    first commit

---
+++ prac_1.py
@@ -0,0 +1,31 @@
+# Function Practice
+def thing():
+ print('Hello')
+print('Fun')
+thing()
+print('Zip')
+thing()
+
+# max-min practice
+big = max('Hello world!')
+print(big)
+
+tiny = min('eeebgree')
+print(tiny)
+
+# calling-function practice
+x = 5
+print('Hello')
+def print_lyrics():
+ print("I'm a lumberjack, and I'm okay.")
+ print('I sleep all night and I work all day.')
+print('Yo')
+print_lyrics()
+x = x + 2
+print(x)
+
+#return practice
+def greet():
+ return "Hello"
+print(greet(),"Usman")
+print(greet(),"Saeed")
